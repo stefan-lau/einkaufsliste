8c9634b6291ec9718f2b3b8365e642d4685c0010
Stefan Lau <stefan@raistech.de>
2024-06-22 21:40:45 +0200

    update api

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ItemController;
 
+/*
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+*/
 
 Route::get('/items', [ItemController::class, 'index']);
 
