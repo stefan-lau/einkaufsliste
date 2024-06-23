be0c59d6b96bddd7014ae8e830567d1a871e1943
Stefan Lau <stefan@raistech.de>
2024-06-23 12:30:06 +0200

    setup vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+import { createApp } from 'vue/dist/vue.esm-bundler.js';
+import Home from './components/Home.vue';
+
+const app = createApp({});
+
+app.component('home-component', Home);
+
+app.mount("#app");
